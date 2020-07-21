fbda91ca8f8eab84987b827bebfcaa817ffc87ac
SebastianN97 <sebbe132132@gmail.com>
2020-07-21 09:40:08 +0200

    Array & Hashes

---
+++ my_group.rb
@@ -0,0 +1,9 @@
+
+
+person_1 = {name: "Sebastian", gender: "male"}
+person_2 = {name: "Jenny", gender: "female"}
+person_3 = {name: "Sara", gender: "female"}
+
+names = ["Sebastian", "Jenny", "Sara"]
+
+

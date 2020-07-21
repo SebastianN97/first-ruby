9648f86cbbc8fbe2b17198ea3d8d1ec3a47886bd
SebastianN97 <sebbe132132@gmail.com>
2020-07-21 17:10:26 +0200

    Finished Array & Hashes challenge

--- my_group.rb
+++ my_group.rb
@@ -1,41 +1,25 @@
 
 
 person_1 = {
-"name" => "Sebastian"
-"gender" => "male"
-"age" => 22
-}
-
+    name: "Sebastian",
+    gender: "male",
+    age: 22
+    }
 person_2 = {
-"name" => "Jenny"
-"gender" => "female"
-"age" => 24
-}
-
+     name: "Jenny",
+    gender: "female",
+    age: 24
+    }
 person_3 = {
-    "name" => "Sara"
-    "gender" => "female"
-    "age": 33
-} 
-
-my_group = [person_1, person_2, person_3]
-
-
-
-#solve this!!!!! 
-person_1.each do |key, value|
-    puts value
-end
+    name: "Sara",
+    gender: "female",
+    age: 33
+    } 
+puts "#{person_1[:name]} is a #{person_1[:gender]} with the age of: #{person_1[:age]}"
+puts "#{person_2[:name]} is a #{person_2[:gender]} with the age of: #{person_2[:age]}"
+puts "#{person_3[:name]} is a #{person_3[:gender]} with the age of: #{person_3[:age]}"
 
 
 
 
-=begin
-
-names = ["Sebastian", "Jenny", "Sara"]
-names.each do |name|
-    puts "Hello #{name}"
-end
-
-
-=end
+####my_group = [person_1, person_2, person_3] .... ARRAY
